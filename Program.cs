c12b3d7d870ab447fc3e81993a0f199a1d4af96f
unknown <legiathinh0508@gmail.com>
2024-08-16 20:53:44 +0700

    UCLN

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the first integer: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second integer: ");
+        int b = int.Parse(Console.ReadLine());
+
+        a = Math.Abs(a);
+        b = Math.Abs(b);
+
+        while (a != b)
+        {
+            if (a > b)
+            {
+                a = a - b;
+            }
+
+            else
+            {
+                b = b - a;
+            }
+        }
+
+        Console.WriteLine("The greatest common divisor is: " + a);
+    }
+}
